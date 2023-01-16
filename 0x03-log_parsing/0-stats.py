400db64e10f9851fc2381a1c89bd02fc3ad1a946
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-01-17 01:16:45 +0300

    fix instruction error on log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,12 +2,10 @@
 import re
 
 input_format = re.compile(r'\d+\.\d+\.\d+\.\d+ - \[.+\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)')
-org_status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
 
 count = 0
 total_file_size = 0
-status_code_count = org_status_code_count.copy()
+status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 while True:
     try:
         line = input()
@@ -15,7 +13,7 @@ while True:
         if match:
             cur_status_code, cur_file_size = match.groups()
             cur_status_code, cur_file_size = int(cur_status_code), int(cur_file_size)
-            if cur_status_code in org_status_code_count.keys():
+            if cur_status_code in status_code_count.keys():
                 status_code_count[cur_status_code] += 1
             total_file_size += cur_file_size
         else:
@@ -25,9 +23,7 @@ while True:
             print(f"File size: {total_file_size}")
             for k, v in status_code_count.items():
                 print(f"{k}: {v}")
-            total_file_size = 0
             count = 0
-            status_code_count = org_status_code_count.copy()
     except KeyboardInterrupt:
         print(f"File size: {total_file_size}")
         for k, v in status_code_count.items():
