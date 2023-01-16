da8788fe7add9cd596ad18037cac9e73517857c3
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-01-17 01:11:40 +0300

    log parsing question

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import re
+
+input_format = re.compile(r'\d+\.\d+\.\d+\.\d+ - \[.+\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)')
+org_status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+
+count = 0
+total_file_size = 0
+status_code_count = org_status_code_count.copy()
+while True:
+    try:
+        line = input()
+        match = input_format.match(line)
+        if match:
+            cur_status_code, cur_file_size = match.groups()
+            cur_status_code, cur_file_size = int(cur_status_code), int(cur_file_size)
+            if cur_status_code in org_status_code_count.keys():
+                status_code_count[cur_status_code] += 1
+            total_file_size += cur_file_size
+        else:
+            continue
+        count += 1
+        if count == 10:
+            print(f"File size: {total_file_size}")
+            for k, v in status_code_count.items():
+                print(f"{k}: {v}")
+            total_file_size = 0
+            count = 0
+            status_code_count = org_status_code_count.copy()
+    except KeyboardInterrupt:
+        print(f"File size: {total_file_size}")
+        for k, v in status_code_count.items():
+            print(f"{k}: {v}")
